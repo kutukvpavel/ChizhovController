125545529825119e6361974bcedac44e1e9e32a4
kutukvpavel <kutukovps@my.msu.ru>
2023-06-23 06:44:20 +0300

    Enable timer init function call in main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -104,6 +104,10 @@ int main(void)
   MX_SPI1_Init();
   MX_TIM2_Init();
   MX_TIM5_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_TIM10_Init();
+  MX_TIM11_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_CRC_Init();
